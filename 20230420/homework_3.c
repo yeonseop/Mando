3c1540d7e4252c4167d1c15926ca30590d455ef9
Taegyun <97517408+qncj5224@users.noreply.github.com>
2023-04-20 18:22:14 +0900

    Update homework_3.c

--- 20230420/homework_3.c
+++ 20230420/homework_3.c
@@ -5,13 +5,13 @@ int main(void) {
 	for (int i = 1; i <= 30; i++) {
 		sum1 = i * i + 1;
 	}
-	printf("%d\n\n", sum1);
+	printf("1번식의 답은 : %d\n\n", sum1);
 
 	for (int i = 10; i <= 30; i++) {
 		for (int j = 0; j < 5; j++) {
 			sum2 = i + j;
 		}
 	}
-	printf("%d", sum2);
+	printf("2번식의 답은 : %d", sum2);
 	return 0;
 }
