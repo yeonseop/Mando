60ad4fef35fcf0c273436179885e1aacb71ac59d
yeonseop <131438111+yeonseop@users.noreply.github.com>
2023-07-07 21:27:41 +0900

    Add files via upload

--- 소스.cpp
+++ 소스.cpp
@@ -1,25 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-void encrypt(char cipher[], int shift);
+#include <string.h>
+#define WORDS 5
+int main(void)
+{
+	int i, index;
+	char dic[WORDS][2][30] = {
+		{"book","책"},
+		{"boy","소년"},
+		{"computer","컴퓨터"},
+		{"lanuguage","언어"},
+		{"rain","비"},
 
-int main(void) {
+	};
+	char word[30];
 
-	char cipher[50];
-	int shift = 3;
-	printf("문자열을 입력하시오:");
-	gets_s(cipher, 50);
-	encrypt(cipher, shift);
-	return 0;
-}
-void encrypt(char cipher[], int shift) {
-	int i = 0;
-	
-	while (cipher[i] != '\0') {
-		if (cipher[i] >= 'A' && cipher[i] <= 'z') {
-			cipher[i] += shift;
-			if (cipher[i] > 'z')
-				cipher[i] -= 26;
+	printf("단어를 입력하시오:");
+	scanf("%s", word);
+
+	index = 0;
+	for (i = 0; i < WORDS; i++) {
+		if (strcmp(dic[index][0], word) == 0) {
+			printf("%s:%s\n", word, dic[index][1]);
+			return 0;
 		}
-		i++;
+		index++;
 	}
-	printf("암호화된 문자열:%s \n", cipher);
+
+	printf("사전에서 발견되지 않았습니다.\n");
+
+	return 0;
 }
